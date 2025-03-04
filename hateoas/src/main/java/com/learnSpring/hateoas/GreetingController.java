a81e5fb263f256f4d6b6ae816d5a12815fc6f0b6
Saket Gulhane <saket.gulhane291@gmail.com>
2025-03-05 02:37:26 +0530

    hateoas - initiall commit

---
+++ hateoas/src/main/java/com/learnSpring/hateoas/GreetingController.java
@@ -0,0 +1,23 @@
+package com.learnSpring.hateoas;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String Template = "Hello, %s!";
+
+    @RequestMapping("/greeting")
+    public HttpEntity<Greeting> greeting(@RequestParam(value="name", defaultValue = "World!") String name){
+            Greeting greeting = new Greeting(String.format(Template, name));
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        return new ResponseEntity<>(greeting, HttpStatusCode.valueOf(200));
+    }
+}
